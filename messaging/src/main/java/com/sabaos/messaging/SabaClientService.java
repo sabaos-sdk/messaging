2a6f3a6b931e3b606d25d9e00abfce7f6e447cf0
Iman Kianian <iriik@yahoo.com>
2019-06-16 11:12:28 +0430

    Revert "replaced Gotify messaging library with Saba messaging library"
    This reverts commit 66c23a1d789000dd50da439c6f653750d991f14d.

--- messaging/src/main/java/com/sabaos/messaging/SabaClientService.java
+++ messaging/src/main/java/com/sabaos/messaging/SabaClientService.java
@@ -14,9 +14,9 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 
-public class ClientService extends IntentService {
+public class SabaClientService extends IntentService {
 
-    public ClientService() {
+    public SabaClientService() {
         super("MyIntentService");
     }
 
